4f288ef32a4c8e85434b5f0af224179e80288b0d
timjkue <timjkue@gmail.com>
2026-01-26 19:49:29 +0100

    types

---
+++ src/types/color.rs
@@ -0,0 +1,13 @@
+pub enum Color {
+    White,
+    Black,
+}
+
+impl Color {
+    pub fn  opposite(&self) -> Color {
+        match self {
+            Color::White => Color::Black,
+            Color::Black => Color::White,
+        }
+    }
+}
